437f14a45514859bb68c7838d53d74b855a474d8
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-04-14 11:05:19 +0200

    Add project files.

---
+++ Mvc2Hockey/Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mvc2Hockey.Models;
+using Mvc2Hockey.ViewModels;
+
+namespace Mvc2Hockey.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public PlayerController(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            var viewModel = _playerRepository.GetAll().Select(p=>new PlayerListViewModel { Id = p.Id, Name = p.Name }).ToList();
+            return View(viewModel);
+        }
+
+
+        // GET
+        public IActionResult View(int id)
+        {
+            var p = _playerRepository.Get(id);
+            var viewModel = new PlayerViewModel { Name =  p.Name, JerseyNumber = p.JerseyNumber};
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Mvc2Hockey/ViewModels/PlayerListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Mvc2Hockey.ViewModels
+{
+    public class PlayerListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PlayerViewModel
+    {
+        public string Name { get; set; }
+        public int JerseyNumber { get; set; }
+    }
+}
